278d4fb499b9eebbbbafcf35f46f2b784e0ad53d
LAURENT DUCAMP <laurent.ducamp@ext.adeo.com>
2017-07-17 11:47:39 +0200

    add source

---
+++ src/main/java/com/adeo/drh/WsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.adeo.drh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsDemoApplication.class, args);
+	}
+}
